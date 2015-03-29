d4d0a890f907122a73eb4e844e64cf2fd7cab28d
Arnaud Rosette <arnaud.rosette@outlook.com>
2015-03-29 22:50:53 +0200

    Microprocessor - lab 7 : code c++ et asm.

---
+++ ELECH473/Labo7/Labo7.cpp
@@ -0,0 +1,160 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "stdafx.h"
+
+unsigned char *parseImgFile(char source_path[], unsigned int *size) {
+	FILE *source_file = NULL;
+	unsigned char *img = NULL;
+	source_file = fopen(source_path, "rb");
+	if (source_file == NULL) {
+		perror("Error while opening the file");
+	}
+
+
+	fseek(source_file, 0, SEEK_END);
+	long int end_of_file = ftell(source_file);
+	fseek(source_file, 0, SEEK_SET);
+
+	unsigned int max_size = (1024 * 1024);
+	*size = 0;
+	img = new unsigned char[max_size*sizeof(unsigned char)];
+
+	unsigned char current_pixel = fgetc(source_file); // char is on 8 bit
+	long int current_location_in_file = ftell(source_file);
+	while (current_location_in_file < end_of_file){
+		img[*size] = current_pixel;
+		*size = *size + 1;
+		if (*size == max_size) {
+			unsigned char *new_memory_area = new unsigned char[(max_size + 1024)*sizeof(unsigned char)];
+			memcpy(new_memory_area, img, max_size);
+			max_size = max_size + 1024;
+			img = new_memory_area;
+		}
+		current_pixel = fgetc(source_file);
+		current_location_in_file = ftell(source_file);
+	}
+
+	fflush(source_file);
+	fclose(source_file);
+
+	return img;
+}
+
+
+void writeResultInFile(char dest_path[], unsigned char *img, int size) {
+	FILE *dest_file = NULL;
+	dest_file = fopen(dest_path, "wb");
+	if (dest_file == NULL) {
+		perror("Error while opening the file");
+	}
+
+	int i = 0;
+	while (i<size) {
+		fputc(img[i], dest_file);
+		i = i + 1;
+	}
+
+	fflush(dest_file);
+	fclose(dest_file);
+}
+
+void asmImageThreshold(unsigned char *src, unsigned char *dest, unsigned char *th, unsigned int asize) {
+
+	unsigned char aVal = 127;
+	unsigned char *anoffset = new unsigned char[16];
+	for (int i = 0; i < 16; ++i) {
+		anoffset[i] = aVal;
+	}
+
+	_asm {
+		mov esi, src;
+		mov edi, dest;
+		mov eax, th;
+		mov ecx, asize;
+		mov edx, anoffset;
+		movdqu xmm6, [edx];
+		movdqu xmm7, [eax];
+		paddb xmm7, xmm6;
+	l1:
+		movdqu xmm0, [esi];
+		paddb xmm0, xmm6;
+		pcmpgtb xmm0, xmm7;
+		movdqu [edi], xmm0;
+		add edi, 16;
+		add esi, 16;
+		sub ecx, 16;
+		cmp ecx, 0;
+		jg l1;
+	}
+	delete anoffset;
+	anoffset = NULL;
+}
+
+void asmRun(char input_file[], char output_file[], unsigned char th) {
+	unsigned int size = 0;
+	unsigned char *src = NULL;
+	src = parseImgFile(input_file, &size);
+
+	unsigned char *dest = new unsigned char[size*sizeof(unsigned char)];
+
+	unsigned char *threshold = new unsigned char[16 * sizeof(unsigned char)];
+	for (int i = 0; i < 16; ++i) {
+		threshold[i] = th;
+	}
+
+	asmImageThreshold(src, dest, threshold, size);
+
+	writeResultInFile(output_file, dest, size);
+	delete threshold;
+	threshold = NULL;
+	delete dest;
+	dest = NULL;
+	delete src;
+	src = NULL;
+}
+
+
+void cImageThreshold(char source_path[], char dest_path[], unsigned char th){
+	unsigned int size = 0;
+	unsigned char *img = NULL;
+
+	//time_t start_time, end_time;
+	//float dt;
+	//start_time = clock();
+
+	img = parseImgFile(source_path, &size);
+
+	unsigned int i = 0;
+	while (i<size) {
+		if (img[i] > th)
+			img[i] = 255;
+		else
+			img[i] = 0;
+		i = i + 1;
+	}
+
+	//end_time = clock();
+	//dt = (end_time - start_time) / (float)(CLOCKS_PER_SEC);
+	//printf("Computing time %f\n", dt);
+	writeResultInFile(dest_path, img, size);
+	delete img;
+	img = NULL;
+}
+
+void cRun(char input_file[], char output_file[], unsigned char th){
+	cImageThreshold(input_file, output_file, th);
+}
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char input_file[] = "C:/Users/arnaud/Documents/ma1-2014-2015/microprocessor/ELECH473/Labo7/Labo7/test.raw";
+	char output_file[] = "C:/Users/arnaud/Documents/ma1-2014-2015/microprocessor/ELECH473/Labo7/Labo7/res-asm.raw";
+	
+	asmRun(input_file, output_file, 64);
+
+	return 0;
+}
